54911d53b24daaeeacf742a08ff11ac3daff13d0
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-26 12:31:50 -0600

    main to app works

---
+++ src/Components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Main = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="group">
+                    <img src="./Images/group.png" alt="" />
+                </div>
+                <div className="search-bar">
+                    <img src='Images/logo.png' alt="logo" />
+                    <input type="search" placeholder='Search'
+                        classNAme="search" />
+                </div>
+
+
+
+            </div>
+
+
+
+        </>
+    )
+}
