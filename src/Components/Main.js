a5fd277c29eb085b62d37c2996f13dd29591704f
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-27 14:37:18 -0600

    makign search

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,6 +8,7 @@ export const Main = () => {
 
     const [url, setUrl] = useState("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d36a1e0d71b87feacf3a52824ecfc4ca&hash=fe3f7e64fa828e449ef675312b956565")
     const [item, setItem] = useState();
+    const [search, setSearch] = useState("");
     useEffect(() => {
         const fetch = async () => {
             const res = await axios.get(url)
@@ -15,20 +16,23 @@ export const Main = () => {
         }
         fetch();
     }, [url])
-
+    const searchMarvel = () => {
+        setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&apikey=d36a1e0d71b87feacf3a52824ecfc4ca`)
+    }
     return (
         <>
             <div className="header">
-                <div className="group">
+                <div className="bg">
                     <img src="./Images/group.png" alt="" />
                 </div>
                 <div className="search-bar">
-                    <img src='Images/logo.png' alt="logo" />
-                    <input type="search" placeholder='Search'
-                        className="search" />
+                    <img src="./Images/logo.png" alt="logo" />
+                    <input type="search" placeholder='Search Here'
+                        className='search'
+                        onChange={e => setSearch(e.target.value)}
+                        onKeyPress={searchMarvel} />
                 </div>
             </div>
-
             <div className="content">
 
                 {
--- src/Components/Marvel.js
+++ src/Components/Marvel.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export const Marvel = () => {
+    const { id } = useParams();
+    const [item, setItem] = useState()
+    const fetch = async () => {
+        const res = await axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=d36a1e0d71b87feacf3a52824ecfc4ca
+        `)
+        setItem(res.data.data.results[0])
+    }
+    fetch();
+
+
     return (
         <>
-        <p>lorem   lorem</p>
+            {
+                (!item) ? "" : (
+                    <div className="box-content">
+                        <div className="right-box">
+                            <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="" />
+                        </div>
+                        <div className="left-box">
+                            <h1>{item.name}</h1>
+                            <h4>{item.description}</h4>
+                        </div>
+
+                    </div>
+                )
+            }
         </>
     )
 }
