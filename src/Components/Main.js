20a600a8b9aae271bffafaf91731d9810ec81abf
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-27 11:12:08 -0600

    working on fetch

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from './Card'
 import axios from 'axios'
-const [url, setUrl] = useState()
+
+import { useState } from 'react'
+
 export const Main = () => {
+
+const [url, setUrl] = useState("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d36a1e0d71b87feacf3a52824ecfc4ca&hash=fe3f7e64fa828e449ef675312b956565")
+
+useEffect(()=> {
+    const fetch=async()=>{
+        const res=await axios.get(url)
+    }
+    fetch();
+},[url])
+
     return (
         <>
             <div className="header">
