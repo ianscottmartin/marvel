f061ac90e0fc380d55b485df5b47e0cce12aaca9
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-28 16:32:11 -0600

    add database

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,8 +8,8 @@ export const Card = ({ data }) => {
                 (data) ? (
                     data.map(item => {
                         return (
-                            <div className="card" key={item.id} 
-                            onClick={() => navigate(`/$item.id}`)}>
+                            <div className="card" key={item.id}
+                                onClick={() => navigate(`/$item.id}`)}>
                                 <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="" />
                                 <div className="title">
                                     <h3>{item.name}</h3>
