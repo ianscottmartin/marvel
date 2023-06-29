859af882b88bbcba8775bd2a00a4201daac9bd6a
Ian Scott Martin <ianscottmartin@gmail.com>
2023-06-28 18:31:26 -0600

    add link to blog

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 export const Main = () => {
 
-    const [url, setUrl] = useState("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=d36a1e0d71b87feacf3a52824ecfc4ca&hash=fe3f7e64fa828e449ef675312b956565")
+    const [url, setUrl] = useState("http://localhost:3000")
     const [item, setItem] = useState();
     const [search, setSearch] = useState("");
     useEffect(() => {
@@ -17,7 +17,7 @@ export const Main = () => {
         fetch();
     }, [url])
     const searchMarvel = () => {
-        setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&apikey=d36a1e0d71b87feacf3a52824ecfc4ca`)
+        setUrl(`http://localhost:3000`)
     }
     return (
         <>
